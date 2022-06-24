2ac3afac5c56e5c78867b782373814866005ecb1
Alex <mereacre@gmail.com>
2022-06-24 14:55:48 +0100

    cleanup

---
+++ docusaurus/sidebars.js
@@ -0,0 +1,8 @@
+module.exports = {
+  someSidebar: {
+    Introduction: ["intro", "control", "capture", "discovery", "storage"],
+    Development: ["compilation", "running", "config", "commands", "deb", "issues"],
+    Deployment: ["installation"],
+    Hardware: ["devices"],
+  },
+};
