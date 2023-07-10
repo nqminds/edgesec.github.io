287b67923b6ee6e094b1ab0c0fd1fc1a50152c94
Alois Klink <alois@nquiringminds.com>
2023-07-10 20:01:25 +0100

    chore(eslint): disable `comma-dangle`
    The `comma-dangle` ESLint rule conflicts with the default Prettier
    trailing commas, at least until Prettier v3.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,6 @@ module.exports = /** @type {import("eslint").ESLint.Options} */ ({
   extends: ["@nqminds/eslint-config-react"],
   rules: {
     "object-curly-spacing": "off", // conflicts with prettier
+    "comma-dangle": "off", // conflicts with prettier v2
   },
 });
